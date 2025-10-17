f26dfb64c0811fef152359d655e7fea54e65a6d9
Riad <kazuha3242@gmail.com>
2025-10-18 01:16:33 +0600

    Header section added with live date and time using date-fns

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import logo from '../../assets/logo.png'
+import { format } from 'date-fns';
 
 const Header = () => {
+    const currentDate = new Date() ;
+    const dayName = format(currentDate,"EEEE") ;
     return (
-        <div className='flex justify-center flex-col items-center mt-7'>
+        <div className='max-w-11/12 mx-auto flex justify-center flex-col items-center mt-7'>
             <img src={logo} alt="Website Logo" />
-            <p className='text-[#706F6F] mt-3'>Journalism Without Fear or Favour</p>
+            <p className='text-accent mt-3'>Journalism Without Fear or Favour</p> 
+            <p className='mt-2 text-accent'>
+                <span className='font-bold text-primary'>{dayName}</span>
+                {format(new Date (), " ,MMMM dd, yyyy")}
+            </p>
         </div>
     );
 };
